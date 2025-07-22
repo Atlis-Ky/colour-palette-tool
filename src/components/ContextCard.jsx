2bb1d2b36f8987e224afe61aaa3f4eb3e24a00c0
Ky <kylesmith97@outlook.com>
2025-07-22 13:13:37 +0100

    fixed visual error in CSS that caused unwanted outlines to display and stick after clicking a colour to copy to clipboard

--- src/components/ContextCard.jsx
+++ src/components/ContextCard.jsx
@@ -7,10 +7,10 @@ const ContextCard = () => (
       <span className="highlight-word">Curated</span> functional colour palettes
     </h1>
     <p className="context-card-text">
-      Explore some handpicked colour palettes, each carefully crafted work well as a set. Click any colour to copy its hex code, or click on the buttons to copy the group CSS properties or JSON formats of each palette. Happy Coding!
+      Explore some handpicked colour palettes, each of them sticking to a theme and designed to complement each other in a set. Click any individual colour to copy its hex code to your clipboard. Click on the buttons to copy the group CSS properties or JSON formats of each palette. Happy Coding!
     </p>
     <p className="context-card-text">
-      Feel free to scroll through all palettes, or use any of the category filters below.
+      Feel free to scroll through all palettes, Search by tag, or use any of the category filters below.
     </p>
   </div>
 );
