b79403e4fce99f40c5e31f24835d9415947396c2
Ky <kylesmith97@outlook.com>
2025-07-23 16:35:27 +0100

    content and logo updates

--- src/components/ContextCard.jsx
+++ src/components/ContextCard.jsx
@@ -7,7 +7,7 @@ const ContextCard = () => (
       <span className="highlight-word">Curated</span> functional colour palettes
     </h1>
     <p className="context-card-text">
-      Explore some handpicked colour palettes, each of them sticking to a theme and designed to complement each other in a set. Click any individual colour to copy its hex code to your clipboard. Click on the buttons to copy the group CSS properties or JSON formats of each palette. Happy Coding!
+      Check out some curated colour palettes that you can use in your own projects, each of them sticking to a theme and designed to complement each other in a set. Click any individual colour to copy its hex code to your clipboard. Click on the buttons to copy the group CSS properties or JSON formats of each palette. Happy Coding!
     </p>
     <p className="context-card-text">
       Feel free to scroll through all palettes, Search by tag, or use any of the category filters below.
